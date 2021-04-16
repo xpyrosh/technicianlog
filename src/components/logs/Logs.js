8aa43dc99b387814547e545d096777d65391aec0
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2021-04-16 19:49:38 -0400

    added materialize preloader for loading animation when loading content from source

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Preloader from "../layout/Preloader";
 import LogItem from "./LogItem";
 
 const Logs = () => {
@@ -20,7 +21,7 @@ const Logs = () => {
 	};
 
 	if (loading) {
-		return <h4>Loading...</h4>;
+		return <Preloader />;
 	}
 
 	return (
