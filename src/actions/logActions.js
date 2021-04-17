cff73cd9ad91aeaaa942026cd0b8273233fe8064
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2021-04-17 15:57:47 -0400

    created redux action and state for adding a log and connected addlogmodal to redux

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
 
 // export const getLogs = () => {
 // 	// Need thunk for async calls
@@ -28,6 +28,34 @@ export const getLogs = () => async (dispatch) => {
 
 		dispatch({
 			type: GET_LOGS,
+			payload: data,
+		});
+	} catch (error) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: error.response.data,
+		});
+	}
+};
+
+// Add new log
+export const addLog = (log) => async (dispatch) => {
+	// Need thunk for async calls
+
+	try {
+		setLoading();
+
+		const res = await fetch("/logs", {
+			method: "POST",
+			body: JSON.stringify(log),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		const data = await res.json();
+
+		dispatch({
+			type: ADD_LOG,
 			payload: data,
 		});
 	} catch (error) {
--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,16 +1,28 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { addLog } from "../../actions/logActions";
 import M from "materialize-css/dist/js/materialize.min.js";
 
-const AddLogModal = () => {
+const AddLogModal = ({ addLog }) => {
 	const [message, setMessage] = useState("");
-	const [attention, setAttention] = useState(false);
 	const [tech, setTech] = useState("");
+	const [attention, setAttention] = useState(false);
 
 	const onSubmit = () => {
 		if (message === "" || tech === "") {
 			M.toast({ html: "Please enter all fields" });
 		} else {
-			console.log(message, tech, attention);
+			const newLog = {
+				message,
+				attention,
+				tech,
+				date: new Date(),
+			};
+
+			addLog(newLog);
+
+			M.toast({ html: `Log added by ${tech}` });
 
 			// Clear fields
 			setMessage("");
@@ -88,4 +100,8 @@ const modalStyle = {
 	height: "75%",
 };
 
-export default AddLogModal;
+AddLogModal.propTypes = {
+	addLog: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addLog })(AddLogModal);
