e02874d4b5166e7cd327cd4676df07d767c2b39c
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2021-04-17 01:34:34 -0400

    set types, create the logactions for logreducer to handle getting logs, log errors and loading

---
+++ src/actions/logActions.js
@@ -0,0 +1,45 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+
+// export const getLogs = () => {
+// 	// Need thunk for async calls
+// 	return async (dispatch) => {
+// 		setLoading();
+
+// 		const res = await fetch("/logs");
+// 		const data = await res.json();
+
+// 		dispatch({
+// 			type: GET_LOGS,
+// 			payload: data,
+// 		});
+// 	};
+// };
+
+// Refactoring above
+export const getLogs = () => async (dispatch) => {
+	// Need thunk for async calls
+
+	try {
+		setLoading();
+
+		const res = await fetch("/logs");
+		const data = await res.json();
+
+		dispatch({
+			type: GET_LOGS,
+			payload: data,
+		});
+	} catch (error) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: error.response.data,
+		});
+	}
+};
+
+// Set loading to true
+export const setLoading = () => {
+	return {
+		type: SET_LOADING,
+	};
+};
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,3 +1,5 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "../actions/types";
+
 const initialState = {
 	logs: null,
 	currect: null,
@@ -7,6 +9,23 @@ const initialState = {
 
 export default (state = initialState, action) => {
 	switch (action.type) {
+		case SET_LOADING:
+			return {
+				...state,
+				loading: true,
+			};
+		case GET_LOGS:
+			return {
+				...state,
+				logs: action.payload,
+				loading: false,
+			};
+		case LOGS_ERROR:
+			console.error(action.payload);
+			return {
+				...state,
+				error: action.payload,
+			};
 		default:
 			return state;
 	}
