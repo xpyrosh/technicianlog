3f1b6d8392a30eb9618f56b19920343dafb99e74
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2021-04-17 20:48:24 -0400

    added delete tech functionality

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -28,6 +28,55 @@ export const getTechs = () => async (dispatch) => {
 	}
 };
 
+// add tech
+export const addTech = (tech) => async (dispatch) => {
+	// Need thunk for async calls
+
+	try {
+		setLoading();
+
+		const res = await fetch("/techs", {
+			method: "POST",
+			body: JSON.stringify(tech),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		const data = await res.json();
+
+		dispatch({
+			type: ADD_TECH,
+			payload: data,
+		});
+	} catch (error) {
+		dispatch({
+			type: TECHS_ERROR,
+			payload: error.response.statusText,
+		});
+	}
+};
+
+// delete tech
+export const deleteTech = (id) => async (dispatch) => {
+	try {
+		setLoading();
+
+		await fetch(`/techs/${id}`, {
+			method: "DELETE",
+		});
+
+		dispatch({
+			type: DELETE_TECH,
+			payload: id,
+		});
+	} catch (error) {
+		dispatch({
+			type: TECHS_ERROR,
+			payload: error.response.statusText,
+		});
+	}
+};
+
 // Set loading to true
 export const setLoading = () => {
 	return {
--- src/components/techs/TechItem.js
+++ src/components/techs/TechItem.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
+import { deleteTech } from "../../actions/techActions";
 import PropTypes from "prop-types";
 
-const TechItem = ({ tech }) => {
+const TechItem = ({ tech, deleteTech }) => {
+	const onDelete = () => {
+		// console.log(tech);
+		deleteTech(tech.id);
+	};
+
 	return (
 		<li className="collection-item">
 			<div>
 				{tech.firstName} {tech.lastName}
-				<a href="#!" className="secondary-content">
+				<a href="#!" className="secondary-content" onClick={onDelete}>
 					<i className="material-icons grey-text">delete</i>
 				</a>
 			</div>
@@ -16,6 +23,7 @@ const TechItem = ({ tech }) => {
 
 TechItem.propTypes = {
 	tech: PropTypes.object.isRequired,
+	deleteTech: PropTypes.func.isRequired,
 };
 
-export default TechItem;
+export default connect(null, { deleteTech })(TechItem);
